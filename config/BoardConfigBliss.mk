0c05306ce3d9e9418669ae70f8194f079883bd51
Jackeagle <jackeagle102@gmail.com>
2018-12-03 10:57:02 -0500

    Revert "vendor/bliss: Dynamically generate kernel headers using bliss generator"
    This reverts commit e0831af9a803dc2bdd1d203d93c3e3ce9b8e120a.

--- config/BoardConfigBliss.mk
+++ config/BoardConfigBliss.mk
@@ -9,7 +9,6 @@ ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
 endif
 
 include vendor/bliss/config/BoardConfigKernel.mk
-include vendor/lineage/config/BoardConfigSoong.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true) 
 include vendor/bliss/config/BoardConfigQcom.mk 
