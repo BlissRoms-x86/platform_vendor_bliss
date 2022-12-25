ReloadedOS/vendor_reloaded

roomservice: Reverse existing project checking order
 * check in wave and lineage manifests, then check in aosp, this allows
   to replace repos that are already tracked in wave (for eg, from a
   different branch)

build/tools/roomservice.py
@@ -165,16 +165,16 @@ def add_to_manifest(repos, fallback_branch=None):
             repo_remote="github"
 
         if is_in_manifest(repo_path):
-            print('%s already exists in the manifest', repo_path)
+            print('%s already exists in the local manifest', repo_path)
             continue
 
         existing_m_project = None
-        if exists_in_tree(mlm, repo_path) != None:
-           existing_m_project = exists_in_tree(mlm, repo_path)
-        elif exists_in_tree(wavem, repo_path) != None:
-             existing_m_project = exists_in_tree(wavem, repo_path)
+        if exists_in_tree(wavem, repo_path) != None:
+            existing_m_project = exists_in_tree(wavem, repo_path)
         elif exists_in_tree(lineagem, repo_path) != None:
-             existing_m_project = exists_in_tree(lineagem, repo_path)
+            existing_m_project = exists_in_tree(lineagem, repo_path)
+        elif exists_in_tree(mlm, repo_path) != None:
+            existing_m_project = exists_in_tree(mlm, repo_path)
 
         if existing_m_project != None:
             if existing_m_project.attrib['path'] == repo['target_path']:
@@ -183,7 +183,7 @@ def add_to_manifest(repos, fallback_branch=None):
                     "name": existing_m_project.attrib['name']
                 }))
 
-        print('Adding dependency:\nRepository: %s\nBranch: %s\nRemote: %s\nPath: %s\n' % (repo_name, repo_branch,repo_remote, repo_path))
+        print('Adding dependency:\nRepository: %s\nBranch: %s\nRemote: %s\nPath: %s\n' % (repo_name, repo_branch, repo_remote, repo_path))
 
         project = ElementTree.Element(
             "project",

BlissRoms-x86/platform_vendor_bliss

 * check in bliss and lineage manifests, then check in aosp, this allows
Signed-off-by: Dmitrii <bankersenator@gmail.com>
Signed-off-by: Jackeagle <jackeagle102@gmail.com>

build/tools/roomservice.py
@@ -166,16 +166,17 @@ def add_to_manifest(repos, fallback_branch=None):
             repo_remote="github"
 
         if is_in_manifest(repo_path):
-            print('%s already exists in the manifest', repo_path)
+            print('%s already exists in the local manifest', repo_path)
             continue
 
         existing_m_project = None
-        if exists_in_tree(mlm, repo_path) != None:
-           existing_m_project = exists_in_tree(mlm, repo_path)
-        elif exists_in_tree(blissm, repo_path) != None:
-             existing_m_project = exists_in_tree(blissm, repo_path)
+
+        if exists_in_tree(blissm, repo_path) != None:
+            existing_m_project = exists_in_tree(blissm, repo_path)
         elif exists_in_tree(lineagem, repo_path) != None:
-             existing_m_project = exists_in_tree(lineagem, repo_path)
+            existing_m_project = exists_in_tree(lineagem, repo_path)
+        elif exists_in_tree(mlm, repo_path) != None:
+            existing_m_project = exists_in_tree(mlm, repo_path)
 
         if existing_m_project != None:
             if existing_m_project.attrib['path'] == repo['target_path']:
@@ -184,7 +185,7 @@ def add_to_manifest(repos, fallback_branch=None):
                     "name": existing_m_project.attrib['name']
                 }))
 
-        print('Adding dependency:\nRepository: %s\nBranch: %s\nRemote: %s\nPath: %s\n' % (repo_name, repo_branch,repo_remote, repo_path))
+        print('Adding dependency:\nRepository: %s\nBranch: %s\nRemote: %s\nPath: %s\n' % (repo_name, repo_branch, repo_remote, repo_path))
 
         project = ElementTree.Element(
             "project",